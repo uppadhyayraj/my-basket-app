775352d8cd8828f4e8d7b5fc42a7d3dc693c3a14
mjalav <mj.alavi@bidone.co.nz>
2026-01-31 23:49:15 +1300

    fix: resolve API test failures...

--- my-basket-api-tests/playwright.config.ts
+++ my-basket-api-tests/playwright.config.ts
@@ -19,8 +19,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Use 1 worker locally to avoid test state conflicts, multiple workers on CI */
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
